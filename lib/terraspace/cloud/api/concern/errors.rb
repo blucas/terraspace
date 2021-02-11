727ccc77a64a8b2681b6c2472d1870cc2e1ea170
Tung Nguyen <tongueroo@gmail.com>
2021-02-11 21:18:21 +0000

    move valid? to var base

--- lib/terraspace/cloud/api/concern/errors.rb
+++ lib/terraspace/cloud/api/concern/errors.rb
@@ -8,20 +8,5 @@ module Terraspace::Cloud::Api::Concern
       $stderr.puts "ERROR: #{result["errors"]}"
       $stderr.puts "Your current settings. org: #{@org} project: #{@project}"
     end
-
-    def valid?
-      if @options[:type] == "deployment" && !@options[:stack]
-        puts "ERROR: When using --type deployment, please specify the env. IE: --stack NAME"
-        return false
-      elsif @options[:type] == "stack" && !@options[:stack]
-        puts "ERROR: When using --type stack, please specify the env. IE: --stack NAME"
-        return false
-      elsif @options[:type] == "env" && @options[:stack] # in case user accidentally specifies stack. env is inferred
-        puts "ERROR: When using --type env, please specify the env. IE: --env NAME."
-        puts "Note: you can also not specify it and the current TS_ENV is used."
-        return false
-      end
-      true
-    end
   end
 end
--- lib/terraspace/cloud/var/base.rb
+++ lib/terraspace/cloud/var/base.rb
@@ -1,4 +1,18 @@
 module Terraspace::Cloud::Var
   class Base < Terraspace::Cloud::AbstractBase
+    def valid?
+      if @options[:level] == "deployment" && !@options[:stack]
+        puts "ERROR: When using --type deployment, please specify the env. IE: --stack NAME"
+        return false
+      elsif @options[:level] == "stack" && !@options[:stack]
+        puts "ERROR: When using --type stack, please specify the env. IE: --stack NAME"
+        return false
+      elsif @options[:level] == "env" && @options[:stack] # in case user accidentally specifies stack. env is inferred
+        puts "ERROR: When using --type env, please specify the env. IE: --env NAME."
+        puts "Note: you can also not specify it and the current TS_ENV is used."
+        return false
+      end
+      true
+    end
   end
 end
